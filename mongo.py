a9bab7d6f53c531c77af06586797987c3662e96b
JocelynVelarde <a01285780@itesm.mx>
2024-12-07 15:13:57 -0600

    added escaped chars

--- mongo.py
+++ mongo.py
@@ -1,8 +1,9 @@
 from pymongo.mongo_client import MongoClient
 import streamlit as st
+from urllib.parse import quote_plus
 
-username = st.secrets["mongo"]["username"]
-password = st.secrets["mongo"]["password"]
+username = quote_plus(st.secrets["mongo"]["username"])
+password = quote_plus(st.secrets["mongo"]["password"])
 cluster_url = st.secrets["mongo"]["cluster_url"]
 
 uri = f"mongodb+srv://{username}:{password}@{cluster_url}/?retryWrites=true&w=majority&appName=Cluster0"
